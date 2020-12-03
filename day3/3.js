6ab7e0368fde18bd4c930ae1fe8fea60f8b9029a
Jared <jarednkrause@gmail.com>
2020-12-03 10:51:33 -0500

    cleanup

--- day3/3.js
+++ day3/3.js
@@ -4,9 +4,8 @@ const inputs = file.split(/\r?\n/);
 
 let trees = 0;
 let sled = 0;
-for (i in inputs) {
+for (let i in inputs) {
   sled = i * 3;
-  if (inputs[i][sled % inputs[i].length] === '#') trees++
+  if (inputs[i][sled % inputs[i].length] === '#') trees++;
 }
-
-console.log(trees)
+console.log(trees);
--- day3/3b.js
+++ day3/3b.js
@@ -2,26 +2,19 @@ const fs = require('fs');
 const file = fs.readFileSync('./input.txt', 'UTF-8');
 const inputs = file.split(/\r?\n/);
 
-let slopes = [
-    [1, 1],
-    [3, 1],
-    [5, 1],
-    [7, 1],
-    [1, 2]
-  ];
-
+let slopes = [[1, 1],[3, 1],[5, 1],[7, 1],[1, 2]];
 let total = 1;
-for (slope in slopes) {
+
+for (let slope in slopes) {
   let coords = slopes[slope];
   let trees = 0;
-  for (i in inputs) {
-    col = i * coords[0];
+  for (let i in inputs) {
+    let col = i * coords[0];
     let row = i * coords[1]
     if (!inputs[row]) break;
     if (inputs[row][col % inputs[i].length] === '#') trees++;
   }
-  total = total * trees;
+  total *= trees;
 }
 
-
-console.log(total)
+console.log(total);
