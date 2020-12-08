b0c3f5a71931a6f094459ddca2025b44516c8ad5
Jared <jarednkrause@gmail.com>
2020-12-08 11:53:58 -0500

    refactor

--- day8/8b.js
+++ day8/8b.js
@@ -2,42 +2,61 @@ const fs = require('fs');
 const file = fs.readFileSync('./input.txt', 'UTF-8');
 const inputs = file.split(/\r?\n/);
 
-let acc = 0;
+let state = new State();
+let loopState = new LoopState();
+let swapped = new Map();
 
-let nopORjmp = new Map();
-
-(function checkNopesJumps() {
-  let counter = 0;
-  let once = false;
-  acc = 0;
-
-  for (var i = 0; i < inputs.length; i++) {
-    if (counter === inputs.length) break;
-    if (!inputs[i]) return;
-    counter++;
-    let instruction = inputs[i].split(" ");
-    switch (instruction[0]) {
-      case "acc":
-        acc += Number(instruction[1])
-        break;
-      case "jmp":
-        if (!nopORjmp.has(i) && !once) {
-          once = true;
-          nopORjmp.set(i, instruction[0])
-        } else {
-          i += Number(instruction[1] - 1)
-        }
-        break;
-      case "nop":
-        if (!nopORjmp.has(i) && !once) {
-          once = true;
-          nopORjmp.set(i, instruction[0])
-          i += Number(instruction[1] - 1)
-        }
-        break;
-    }
+function State() {
+  return {
+    pos: 0,
+    acc: 0
   }
-  checkNopesJumps();
-})()
+}
 
-console.log(acc)
+function LoopState() {
+  return {
+    visited: new Set(),
+    once: false
+  }
+}
+
+function acc(val) {
+  state.acc += Number(val)
+}
+
+function jmp(val) {
+  state.pos += Number(val)
+}
+
+function swap(val) {
+  let success = !swapped.has(state.pos) && !loopState.once
+  if (success) {
+    loopState.once = true;
+    swapped.set(state.pos, val)
+  }
+  return success
+}
+
+while (true) {
+  if (!inputs[state.pos]) break;
+  if (loopState.visited.has(state.pos)) {
+    state = new State();
+    loopState = new LoopState();
+  }
+  loopState.visited.add(state.pos);
+  let instruction = inputs[state.pos].split(" ");
+  switch (instruction[0]) {
+    case "acc":
+      acc(instruction[1])
+      jmp(1)
+      break;
+    case "jmp":
+      swap(instruction[0]) ? jmp(1) : jmp(instruction[1])
+      break;
+    case "nop":
+      swap(instruction[0]) ? jmp(instruction[1]) : jmp(1)
+      break;
+  }
+}
+
+console.info(state)
