53877252b04a223f1a56241ea18263d1f3145230
kravse <jared@cohere.ai>
2022-12-05 13:28:37 -0500

    day5lol

---
+++ 2022/4/day4b.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const article = fs
+  .readFileSync(`./input.txt`, "utf8")
+  .split("\n")
+  .filter((val) => !!val);
+
+let count = 0;
+article.forEach((val) => {
+  let halves = val.split(",");
+  const left = halves[0].split("-");
+  const right = halves[1].split("-");
+
+  if (
+    !(
+      Number(left[1]) < Number(right[0]) ||
+      Number(right[0]) > Number(left[1]) ||
+      Number(right[1]) < Number(left[0]) ||
+      Number(left[0]) > Number(right[1])
+    )
+  ) {
+    count++;
+  }
+});
+console.log(count);
---
+++ 2022/5/day5.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const article = fs
+  .readFileSync(`./input.txt`, "utf8")
+  .split("\n")
+  .filter((val) => !!val);
+const indexOfFirstMove = article.indexOf(
+  article.find((val) => val.startsWith("move"))
+);
+let stack = article.slice(0, indexOfFirstMove);
+let instructions = article.slice(indexOfFirstMove, article.length);
+
+// lmao
+let stacks = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+};
+
+stack.forEach((val) => {
+  let split = val.match(/.{1,4}/g);
+  split.forEach((val, i) => {
+    val = val.trim().replace("[", "").replace("]", "");
+    if (val.trim() && isNaN(val)) {
+      stacks[i + 1].push(val);
+    }
+  });
+});
+for (key in stacks) {
+  stacks[key] = stacks[key].reverse();
+}
+const parseInstructions = (str) => {
+  return {
+    move: Number(str.slice(4, str.indexOf("from")).trim()),
+    from: Number(str.slice(str.indexOf("from ") + 5, str.indexOf("to")).trim()),
+    to: Number(str.slice(str.indexOf("to ") + 3, str.length).trim()),
+  };
+};
+instructions.forEach((instruction) => {
+  let i = parseInstructions(instruction);
+  for (let x = 0; x < i.move; x++) {
+    if (stacks[i.from].length > 0) {
+      let val = stacks[i.from].pop();
+      stacks[i.to].push(val);
+    }
+  }
+});
+let result = "";
+for (key in stacks) {
+  result = `${result}${stacks[key].pop()}`;
+}
+console.log(result);
