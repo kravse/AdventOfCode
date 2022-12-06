ffc02c604f4c9d064af53e3d0846684e28cc3331
kravse <jared@cohere.ai>
2022-12-06 10:57:57 -0500

    day6

---
+++ 2022/6/day6.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const stream = fs
+  .readFileSync(`./input.txt`, "utf8")
+  .split("")
+  .filter((val) => !!val && val !== `\n`);
+const pattern = /^(?:([A-Za-z])(?!.*\1))*$/;
+let count = 0;
+const messageMarker = [];
+const messageMarkerLen = 4;
+
+/* part b */
+// let messageMarkerLen = 14;
+
+stream.every((val) => {
+  count++;
+  messageMarker.length > messageMarkerLen - 1 && messageMarker.shift();
+  messageMarker.push(val);
+  return messageMarker.length < messageMarkerLen
+    ? true
+    : !pattern.test(messageMarker.join(""));
+});
+
+console.log(count);
